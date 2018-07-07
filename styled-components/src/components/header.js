8b41abcfef02f89a73dfcc6edf8099fc401d60d8
Artem Sapegin <artem@sapegin.ru>
2018-07-07 21:20:47 +0200

    Finished styled-components example

--- styled-components/src/components/header.js
+++ styled-components/src/components/header.js
@@ -1,33 +1,35 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from 'styled-components'
+
+const Container = styled.header`
+	background: rebeccapurple;
+	margin-bottom: 1.45rem;
+`
+
+const Content = styled.div`
+	margin: 0 auto;
+	max-width: 960px;
+	padding: 1.45rem 1.0875rem;
+`
+
+const Heading = styled.h1`
+	margin: 0;
+`
+
+const HeaderLink = styled(Link)`
+	color: white;
+	text-decoration: none;
+`
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
+	<Container>
+		<Content>
+			<Heading>
+				<HeaderLink to="/">{siteTitle}</HeaderLink>
+			</Heading>
+		</Content>
+	</Container>
 )
 
 export default Header
